a08dfb1090d7156ef15a6dd7409f5654c7870415
Juan Cruz Cáceres <106564947+caceresjuancruz@users.noreply.github.com>
2024-08-01 10:26:11 -0300

    Initial commit

---
+++ app/[lng]/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>
+          Next TS Starter with Internationalization (i18n)
+        </h1>
+      </div>
+    </section>
+  )
+}
---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+//Types and interfaces needed for components/pages
+export interface LayoutProps {
+  params: { lng: string }
+  children: React.ReactNode
+}
