527c7e561e3b483c50d3aee25f30c53e935bda7a
Juan Cruz Cáceres <caceresjuan.dev@gmail.com>
2024-08-05 05:53:44 -0300

    Add prettier script

--- actions/example/example.action.ts
+++ actions/example/example.action.ts
@@ -5,7 +5,7 @@ import { ExampleParams, exampleSchema } from './example.types'
 
 export async function example(params: ExampleParams) {
   try {
-    //always validate the input data before using it 
+    //always validate the input data before using it
     const validatedValues = exampleSchema.parse(params)
 
     // Your code here
--- actions/example/example.types.d.ts
+++ actions/example/example.types.d.ts
@@ -1,8 +1,7 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const exampleSchema = z.object({
-    example: z
-      .string()
-});
+  example: z.string()
+})
 
-export type ExampleParams = z.infer<typeof exampleSchema>;
+export type ExampleParams = z.infer<typeof exampleSchema>
